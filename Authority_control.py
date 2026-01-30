f389dafde42ca81eba3ea6f52b582bd502795357
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-01-30 23:01:35 +0800

    Refactor add_authority_control_template logic
    Refactor add_authority_control_template function to improve logic for inserting authority control template.

--- Authority_control.py
+++ Authority_control.py
@@ -59,11 +59,10 @@ def has_authority_control(page, AUTHORITY_CONTROL_ID:tuple) -> bool:
 
 def add_authority_control_template(page):
     text = page.text
-    place = 0
-    find = BOTTOM_PATTERN.findall(text)
-    if find != []:
-        text = BOTTOM_PATTERN.sub("", text)
-        text += f"\n\{\{Authority control\}\}{'\n'.join([i.group() for i in find)])}"
+    match = BOTTOM_PATTERN.search(text)
+    if match != None:
+        place = match.start()
+        f"{text[:place]}\{\{Authority control\}\}{text[place:]}"
     save(site, page, text, "根據維基數據資料添加[[Template:Authority control|權威控制模板]]")
 
 def need_authority_control_template(page, AUTHORITY_CONTROL_ID:tuple) -> bool:
