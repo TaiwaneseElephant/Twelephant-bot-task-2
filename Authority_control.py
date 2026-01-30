16ce7f886cc808b31285b5c809fbcdedc2cbbab1
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-01-30 20:19:16 +0800

    Exclude redirect pages from authority control check
    Prevent authority control check on redirect pages.

--- Authority_control.py
+++ Authority_control.py
@@ -55,8 +55,6 @@ def has_authority_control(page, AUTHORITY_CONTROL_ID:tuple) -> bool:
     return False
 
 def need_authority_control_template(page, AUTHORITY_CONTROL_ID:tuple) -> bool:
-    if page.isRedirectPage():
-        return False
     text = page.text
     for i in ("{{Authority control", "{{authority control", "{{規範控制", "{{规范控制", "{{權威控制", "{{权威控制"):
         if i in text:
@@ -75,7 +73,7 @@ if __name__ == "__main__":
     except:
         viewed = set()
         log = []
-    for page in pagegenerators.AllpagesPageGenerator(site = site):
+    for page in pagegenerators.AllpagesPageGenerator(site = site, includeredirects = False):
         title = page.title()
         if title in viewed:
             continue
