8a08b075cc4328502b68980d180f483302edba00
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-01 23:17:52 +0800

    Update Authority_control.py

--- Authority_control.py
+++ Authority_control.py
@@ -3,17 +3,7 @@ import pywikibot
 from pywikibot import pagegenerators, textlib
 import json
 import re
-import os
-import os.path
-import datetime
 import time
-
-AUTHORITY_CONTROL_ID = {
-  268, 214, 7859, 3372, 6804, 1907, 4186, 2092, 1908, 1707, 6829, 2349, 6792, 227, 1960, 347, 1248, 244, 1225, 2041, 409, 2750, 650, 350, 781, \
-  3430, 3544, 1315, 245, 1986, 7902, 651, 791, 7303, 3563, 4055, 3223, 4423, 3723, 3993, 3562, 2980, 4038, 3920, 4143, 3863, 3601, 902, 886, \
-  3065, 1362, 691, 1890, 950, 9984, 3348, 1375, 8189, 1736, 396, 8034, 349, 271, 5034, 1368, 1006, 1695, 7293, 1003, \
-  947, 906, 5587, 7314, 1048, 2558
-}
 
 BOTTOM_PATTERN = re.compile(r"\{\{\s*(?:(?:[Tt](?:emplate)?|模板)\s*:)?\s*(?:DEFAULTSORT:.*?|[Ss]tub(?:\|.*?)?|.*?-stub(?:\|.*?)?|.*?小作品(?:\|.*?)?|小條目(?:\|.*?)?)\s*\}\}", flags = re.DOTALL)
 
@@ -101,63 +91,18 @@ def main(limit:int = float("inf")):
     except:
       print("Failed to load config")
       return
-    log = []
-    if os.path.exists("task-2-viewed.json"):
-        try:
-            with open("task-2-viewed.json", "r", encoding = "utf-8") as f:
-                temp = json.load(f)
-            assert isinstance(temp, dict)
-            all_viewed = {}
-            viewed = set()
-            for i in temp:
-                try:
-                    if datetime.datetime.now(datetime.UTC) - datetime.datetime.strptime(i, "%Y-%m-%d").replace(tzinfo=timezone.utc) < datetime.timedelta(days = 30):
-                        all_viewed[i] = temp[i]
-                        viewed.update(temp[i])
-                except Exception as e:
-                    print(f"Failed to update all_viewed set, title: {temp[i]}, error:{e}")
-            del temp
-            try:
-                with open("task-2-viewed-temp.json", "w", encoding = "utf-8") as f:
-                    json.dump(all_viewed, f, ensure_ascii = False)
-                os.replace("task-2-viewed-temp.json", "task-2-viewed.json")
-            except Exception as e:
-                print(f"Failed to update task-2-viewed.json, content:\n{all_viewed}, error:{e}")
-        except:
-            all_viewed = {}
-            viewed = set()
-    else:
-        all_viewed = {}
-        viewed = set()
-    new_viewed = []
+    pages_has_template = [page.title() for page in pwb.Page(site, "Template:Authority control").embeddedin(namespaces =0)]
     for page in pagegenerators.AllpagesPageGenerator(site = site, namespace = 0, filterredir = False):
         title = page.title()
-        if title in viewed:
+        if title in pages_has_template:
             continue
         if need_authority_control_template(page) and page.botMayEdit():
             success = add_authority_control_template(site, page)
             if not success:
               continue
-            log.append(title)
-            if len(log) >= limit:
-                break
             if not check_switch(site):
                 break
-        viewed.add(title)
-        new_viewed.append(title)
-        if len(new_viewed) == 50:
-            time_now = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%d")
-            if time_now in all_viewed:
-                all_viewed[time_now].extend(new_viewed)
-            else:
-                all_viewed[time_now] = new_viewed
-            with open("task-2-viewed-temp.json", "w", encoding = "utf-8") as f:
-                json.dump(all_viewed, f, ensure_ascii = False)
-            new_viewed = []
-            os.replace("task-2-viewed-temp.json", "task-2-viewed.json")
-            if not check_switch(site):
-                break
-        time.sleep(10)
+            time.sleep(10)
 
 if __name__ == "__main__":
     main()
