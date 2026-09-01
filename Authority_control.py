770f4ebf41a562d2757f532cd3e8b4ffee8c41b6
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-01 23:47:35 +0800

    Refactor pywikibot references to use alias pwb

--- Authority_control.py
+++ Authority_control.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import pywikibot
+import pywikibot as pwb
 from pywikibot import pagegenerators, textlib
 import json
 import re
@@ -21,19 +21,19 @@ def save(site, page, text:str, summary:str = "", minor:bool = True, max_retry_ti
             page.text = text
             page.save(summary, minor = minor, bot=True)
             return True
-        except pywikibot.exceptions.EditConflictError as e:
+        except pwb.exceptions.EditConflictError as e:
             print(f"Warning! There is an edit conflict on page '{page.title()}'!")
             oringinal_text = page.get(force = True, get_redirect = True)
-        except pywikibot.exceptions.LockedPageError as e:
+        except pwb.exceptions.LockedPageError as e:
             print(f"Warning! The edit attempt on page '{page.title()}' was disallowed because the page is protected!")
             break
-        except pywikibot.exceptions.AbuseFilterDisallowedError as e:
+        except pwb.exceptions.AbuseFilterDisallowedError as e:
             print(f"Warning! The edit attempt on page '{page.title()}' was disallowed by the AbuseFilter!")
             break
-        except pywikibot.exceptions.SpamblacklistError as e:
+        except pwb.exceptions.SpamblacklistError as e:
             print(f"Warning! The edit attempt on page '{page.title()}' was disallowed by the SpamFilter because the edit add blacklisted URL!")
             break
-        except pywikibot.exceptions.TitleblacklistError as e:
+        except pwb.exceptions.TitleblacklistError as e:
             print(f"Warning! The edit attempt on page '{page.title()}' was disallowed because the title is blacklisted!")
             break
     print(f"The attempt to edit the page '{page.title()}' was stopped because of the error below:\n{e}\nThe edit is '{text[:100]}', and the summary is '{summary}'.")
@@ -41,14 +41,14 @@ def save(site, page, text:str, summary:str = "", minor:bool = True, max_retry_ti
 
 def check_switch(site) -> bool:
     try:
-        switch_page = pywikibot.Page(site, "User:Twelephant-bot/task/2/config.json")
+        switch_page = pwb.Page(site, "User:Twelephant-bot/task/2/config.json")
         return json.loads(switch_page.text)["Enable"]
     except:
         return False
 
 def has_authority_control(page, AUTHORITY_CONTROL_ID) -> bool:
     try:
-        item = pywikibot.ItemPage.fromPage(page)
+        item = pwb.ItemPage.fromPage(page)
         repo = item.repo
         claims = item.get().get("claims", {})
         for prop_id in claims:
@@ -57,7 +57,7 @@ def has_authority_control(page, AUTHORITY_CONTROL_ID) -> bool:
                     return True
             except:
                 pass
-    except pywikibot.exceptions.NoPageError:
+    except pwb.exceptions.NoPageError:
         pass
     return False
 
@@ -80,22 +80,22 @@ def need_authority_control_template(page, AUTHORITY_CONTROL_TEMPLATE, AUTHORITY_
     return has_authority_control(page, AUTHORITY_CONTROL_ID)
 
 def main(limit:int = float("inf")):
-    site = pywikibot.Site("wikipedia:zh")
+    site = pwb.Site("wikipedia:zh")
     try:
-      config = json.loads(pywikibot.Page(site, "User:Twelephant-bot/task/2/config.json").text)
+      config = json.loads(pwb.Page(site, "User:Twelephant-bot/task/2/config.json").text)
       AUTHORITY_CONTROL_ID = config["authority control id"]
       AUTHORITY_CONTROL_TEMPLATE = config["template"]
+      updatepage = config["updatepage"]
       summary = config["summary"]
       if not config["Enable"]:
         return
     except:
       print("Failed to load config")
       return
-    pages_has_template = [page.title() for page in pwb.Page(site, "Template:Authority control").embeddedin(namespaces =0)]
-    for page in pagegenerators.AllpagesPageGenerator(site = site, namespace = 0, filterredir = False):
-        title = page.title()
-        if title in pages_has_template:
-            continue
+    pages_have_template = set([page.title() for page in pwb.Page(site, "Template:Authority control").embeddedin(namespaces =0)])
+    pages_need_authority_control_template = set(json.loads(pwb.Page(site, updatepage).text)) - pages_have_template
+    for title in pages_need_authority_control_template:
+        page = pwb.Page(site, title)
         if need_authority_control_template(page, AUTHORITY_CONTROL_TEMPLATE, AUTHORITY_CONTROL_ID) and page.botMayEdit():
             success = add_authority_control_template(site, page, summary)
             if success:
