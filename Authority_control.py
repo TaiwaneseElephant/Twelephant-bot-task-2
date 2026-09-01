717416270a7e6fcbb377f31bfe1dac631d152367
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-01 23:14:55 +0800

    Update Authority_control.py

--- Authority_control.py
+++ Authority_control.py
@@ -16,7 +16,6 @@ AUTHORITY_CONTROL_ID = {
 }
 
 BOTTOM_PATTERN = re.compile(r"\{\{\s*(?:(?:[Tt](?:emplate)?|模板)\s*:)?\s*(?:DEFAULTSORT:.*?|[Ss]tub(?:\|.*?)?|.*?-stub(?:\|.*?)?|.*?小作品(?:\|.*?)?|小條目(?:\|.*?)?)\s*\}\}", flags = re.DOTALL)
-AUTHORITY_CONTROL_TEMPLATE_PATTERN = re.compile(r"\{\{\s*(?:(?:[Tt](?:emplate)?|模板)\s*:)?\s*(?:[Aa]uthority [Cc]ontrol|[Aa]c|[Aa]utC|[規规][範范]控制|[權权]威控制|[Nn]ormdaten)(?:\|.*?)?\s*\}\}", flags = re.DOTALL)
 
 def save(site, page, text:str, summary:str = "", minor:bool = True, max_retry_times:int = 3):
     e = None
