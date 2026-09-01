064fb04b3cfb4a9fd09a2da65c6f7dd104183f15
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-01 23:42:11 +0800

    Create update.py

---
+++ update.py
@@ -0,0 +1,31 @@
+from pywikibot.data import sparql
+from pywikibot import pagegenerators
+import pywikibot as pwb
+import json
+import time
+
+def main():
+    site = pywikibot.Site("wikipedia:zh")
+    try:
+      config = json.loads(pywikibot.Page(site, "User:Twelephant-bot/task/2/config.json").text)
+      template = config["template"]
+      AUTHORITY_CONTROL_ID = config["authority control id"]
+      query_string = config["query string"]
+      updatepage = config["updatepage"]
+      updatesummary = config["updatesummary"]
+      if not config["Enable"]:
+        return
+    except:
+          print("Failed to load config")
+      return
+    properties = (" ".join(["wdt:P%d" % i for i in AUTHORITY_CONTROL_ID]))
+    SparqlQuery = sparql.SparqlQuery()
+    site = pwb.Site("wikipedia:zh")
+    result = SparqlQuery.query(query=QUERY % (propertie))
+    pages = set([page["title"] for page in result])- set([page.title() for page in pwb.Page(site, template).embeddedin(namespaces =0)])
+    log_page = pwb.Page(site, updatepage)
+    log_page.text = json.dumps(pages)
+    log.save(minor=True, bot=True, summary=updatesummary)
+    time.sleep(2592000)
+
+main()
