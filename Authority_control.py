b466243b2e34407b303069f28348a199eff60374
臺灣象象 <yellow.elephant.taiwan@gmail.com>
2026-09-03 22:39:17 +0800

    Update Authority_control.py

--- Authority_control.py
+++ Authority_control.py
@@ -8,7 +8,7 @@ import json
 
 BOTTOM_PATTERN = re.compile(r"\{\{\s*(?:(?:[Tt](?:emplate)?|模板)\s*:)?\s*(?:DEFAULTSORT:.*?|[Ss]tub(?:\|.*?)?|.*?-stub(?:\|.*?)?|.*?小作品(?:\|.*?)?|小條目(?:\|.*?)?)\s*\}\}", flags = re.DOTALL)
 
-def save(site, page, func = lambda x:x, summary:str = "", max_retry_times:int = 3):
+def save(site, page, func = lambda x:x, summary:str = "", max_retry_times:int = 3, **kargs) -> bool:
     e = None
     if page.exists():
         oringinal_text = page.get(force = True, get_redirect = False)
@@ -16,12 +16,12 @@ def save(site, page, func = lambda x:x, summary:str = "", max_retry_times:int = 
       return False
     for _ in range(max_retry_times):
         try:
-            page.text = func(oringinal_text, site)
+            page.text = func(oringinal_text, site, **kargs)
             page.save(summary, minor = True, bot=True)
             return True
         except pwb.exceptions.EditConflictError as e:
             print(f"Warning! There is an edit conflict on page '{page.title()}'!", flush=True)
-            oringinal_text = page.get(force = True, get_redirect = True)
+            oringinal_text = page.get(force = True, get_redirect = False)
         except pwb.exceptions.LockedPageError as e:
             print(f"Warning! The edit attempt on page '{page.title()}' was disallowed because the page is protected!", flush=True)
             break
@@ -44,20 +44,14 @@ def check_switch(site) -> bool:
     except:
         return False
 
-def add_authority_control_template(text, site) -> None:
+def add_authority_control_template(text, site, template) -> str:
     cats = textlib.getCategoryLinks(text, site)
     text = textlib.removeCategoryLinks(text, site)
     match = BOTTOM_PATTERN.findall(text)
     text = BOTTOM_PATTERN.sub("", text)
-    text = f"{text.rstrip()}\n{{{{Authority control}}}}\n{'\n'.join(match)}"
+    text = f"{text.strip()}\n{{{{{template}}}}}\n{'\n'.join(match)}"
     text = textlib.replaceCategoryLinks(text, cats, site, add_only = True)
     return text
-
-def hasTemplate(page, AUTHORITY_CONTROL_TEMPLATE) -> bool:
-    for template in page.itertemplates(namespaces=10):
-        if template.title() == AUTHORITY_CONTROL_TEMPLATE:
-            return True
-    return False
 
 def getSparqlQuery(AUTHORITY_CONTROL_ID:list, query_string:str) -> set:
     SparqlQuery = sparql.SparqlQuery()
@@ -77,7 +71,7 @@ def getSparqlQuery(AUTHORITY_CONTROL_ID:list, query_string:str) -> set:
         print(len(result), flush=True)
     return pages
 
-def main(limit:int = float("inf")):
+def main(limit:int = float("inf")) -> None:
     site = pwb.Site("wikipedia:zh")
     try:
         config = json.loads(pwb.Page(site, "User:Twelephant-bot/task/2/config.json").text)
@@ -98,14 +92,14 @@ def main(limit:int = float("inf")):
         except Exception as e:
             print(f"SparqlQueryError: {e}", flush=True)
             time.sleep(60)
-    pages_need_authority_control_template = pages_need_authority_control_template
     print(len(pages_need_authority_control_template), flush=True)
+    template = pwb.Page(site, AUTHORITY_CONTROL_TEMPLATE, ns=10)
     t = 0
     for title in pages_need_authority_control_template:
         page = pwb.Page(site, title)
-        if not page.botMayEdit() or page.isRedirectPage() or page.isDisambig() or hasTemplate(page, AUTHORITY_CONTROL_TEMPLATE):
+        if not page.botMayEdit() or page.isRedirectPage() or page.isDisambig() or template in page.templates(namespaces=10):
             continue
-        success = save(site, page, add_authority_control_template, summary)
+        success = save(site, page, add_authority_control_template, summary, template = AUTHORITY_CONTROL_TEMPLATE)
         if success:
             print(title)
             t += 1
